d3ce3cf7b213fa845066aea71d331551d852bcb0
EdwarAJA <edwajayo89@gmail.com>
2024-11-10 22:33:32 -0500

    Front Recomendaciones y Tendencias

--- src/app/components/recomendaciones/crear-editar-recomendaciones/crear-editar-recomendaciones.component.ts
+++ src/app/components/recomendaciones/crear-editar-recomendaciones/crear-editar-recomendaciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -6,11 +6,14 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recomendaciones } from '../../../models/Recomendaciones';
 import { RecomendacionesService } from '../../../services/recomendaciones.service';
 import { Tendencia } from '../../../models/Tendencia';
@@ -26,12 +29,13 @@ import { TendenciasService } from '../../../services/tendencias.service';
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule,    
+    CommonModule,
   ],
   templateUrl: './crear-editar-recomendaciones.component.html',
   styleUrl: './crear-editar-recomendaciones.component.css'
 })
-export class CrearEditarRecomendacionesComponent implements OnInit{
+export class CrearEditarRecomendacionesComponent{
   form: FormGroup = new FormGroup({});
   listaTendencias: Tendencia[]=[];
   listaUsuarios: Usuarios[]=[];
@@ -43,16 +47,26 @@ export class CrearEditarRecomendacionesComponent implements OnInit{
     private rS: RecomendacionesService,
     private formBuilder: FormBuilder,
     private router: Router,
+    private route: ActivatedRoute,
+
     private uS: UsuarioService,
     private tS: TendenciasService,
   ) {}
   ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      //trae los datos
+      this.init();
+  });
+
     this.form = this.formBuilder.group({
+      hcodigo:[''],
       hUsuario: ['', Validators.required],
       hTendencia: ['', Validators.required],
       hdescripcion: ['', Validators.required],
-      hfechaCreacion: ['', Validators.required],
-      hfechaModificacion: ['', Validators.required],
+      hfechaCreacion: ['', [Validators.required, this.fechaFuturaValidator]],
+      hfechaModificacion: ['', [Validators.required, this.fechaFuturaValidator]],
     });
     this.uS.list().subscribe((data) => {
       this.listaUsuarios = data;
@@ -63,18 +77,48 @@ export class CrearEditarRecomendacionesComponent implements OnInit{
   }
   insertar(): void {
     if (this.form.valid) {
+      this.recomendacion.id_Recomendacion= this.form.value.hcodigo;
       this.recomendacion.descripcion = this.form.value.hdescripcion;
+      // Asigna el idUsuario y idTendencia
+      this.recomendacion.id_Usuario.id = this.form.value.hUsuario;
+      this.recomendacion.id_Tendencia.idTendencia = this.form.value.hTendencia;
       this.recomendacion.fecha_creacion = this.form.value.hfechaCreacion;
       this.recomendacion.fecha_modificacion = this.form.value.hfechaModificacion;
-      // Asigna el idUsuario y idTendencia
-      this.recomendacion.id_Usuario = this.form.value.hUsuario;
-      this.recomendacion.id_Tendencia = this.form.value.hTendencia;
-      this.rS.insert(this.recomendacion).subscribe((data) => {
+      if (this.edicion) {
+        this.rS.update(this.recomendacion).subscribe((data) => {
           this.rS.list().subscribe((data) => {
             this.rS.setList(data);
           });
         });
-        this.router.navigate(['recomendaciones']);
-    } 
+      } else {
+        this.rS.insert(this.recomendacion).subscribe(data=>{
+          this.rS.list().subscribe(data=>{
+            this.rS.setList(data)
+          })
+        });
+      }   
+    }
+    this.router.navigate(['recomendaciones']);
   }
+  init() {
+    if (this.edicion) {
+      this.rS.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          hcodigo: new FormControl(data.id_Recomendacion),
+          hdescripcion: new FormControl(data.descripcion),
+          hfechaCreacion: new FormControl(data.fecha_creacion),
+          hUsuario:  new FormControl(data.id_Usuario.username),
+          hTendencia:  new FormControl(data.id_Tendencia.popularidad),
+          hfechaModificacion: new FormControl(data.fecha_modificacion),
+        });
+      });
+    }
+  }
+
+  fechaFuturaValidator(control: FormControl) {
+    const fechaIngresada = new Date(control.value);
+    const fechaActual = new Date();
+    return fechaIngresada < fechaActual ? { fechaFutura: true } : null;
+  }
+  
 }
--- src/app/models/Recomendaciones.ts
+++ src/app/models/Recomendaciones.ts
@@ -1,8 +1,11 @@
+import { Tendencia } from "./Tendencia"
+import { Usuarios } from "./Usuarios"
+
 export class Recomendaciones{
     id_Recomendacion:number=0
     descripcion:string=""
     fecha_creacion:Date = new Date(Date.now())
     fecha_modificacion:Date=new Date(Date.now())
-    id_Usuario:number=0    
-    id_Tendencia:number=0
+    id_Usuario:Usuarios= new Usuarios()    
+    id_Tendencia:Tendencia= new Tendencia()
 }
