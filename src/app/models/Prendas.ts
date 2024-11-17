7fdf87811ed370233c1a246f6a3ded9addb25bca
CesarTapia10 <u202210918@upc.edu.pe>
2024-11-16 23:23:25 -0500

    cambios reportes 2

--- src/app/models/Prendas.ts
+++ src/app/models/Prendas.ts
@@ -6,7 +6,7 @@ export class Prendas{
       id_usuario: Usuarios=new Usuarios();
       nombre_prenda:string=""
       tipo_prenda:string=""
-      imagen: Uint8Array = new Uint8Array();
+      imagen_url: string=""
       fecha_creacion:Date=new Date(Date.now())
       fecha_modificacion:Date=new Date(Date.now())
 }
